ab853e6aead8481b31a011be412a737fadc06a6b
Chintu <74888598+chin2p@users.noreply.github.com>
2023-09-27 03:24:10 -0400

    stack.c works
    I think we are getting the desired Output:
    handling segmentation fault!
    result after handling seg fault 30!

--- src/stack/stack.c
+++ src/stack/stack.c
@@ -1,10 +1,12 @@
 /*
 * Add NetID and names of all project partners
-*
+* csp126 - Chirantan Patel, 
 */
+
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 
 /* Part 1 - Step 1 and 2: Do your tricks here
  * Your goal must be to change the stack frame of caller (main function)
@@ -15,6 +17,14 @@ void signal_handle(int signalno) {
     printf("handling segmentation fault!\n");
 
     /* Step 2: Handle segfault and change the stack*/
+    int *pSig = &signalno;
+    //Find and replace offset
+    int offset = 15;
+    int *bLen = pSig + offset;
+
+    //Find and replace the length of bytes below
+    *bLen += 2;
+
 }
 
 int main(int argc, char *argv[]) {
@@ -23,6 +33,7 @@ int main(int argc, char *argv[]) {
 
     /* Step 1: Register signal handler first*/
 
+    signal(SIGSEGV, signal_handle);
 
     r2 = *( (int *) 0 ); // This will generate segmentation fault
 
